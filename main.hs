3d3dd34b72270c267c6c2ab8e6706beb00f7a6d5
evanlyu732 <evan.lv@uditech.com.cn>
2023-01-12 16:09:21 +0800

    updates file

--- main.hs
+++ main.hs
@@ -8,8 +8,56 @@ densityTell density
   | density <= 2.2 = "wow 2"
   | otherwise = ":)"
 
+-- head' :: [a] -> a
+-- head' [] = error "no head for empty files"
+-- head' (x: _) = x
+--
+-- case expression of pattern -> result
+--                    pattern -> result
+--                    pattern -> result
+head' :: [a] -> a
+head' xs = case xs of [] -> error "no head for empty files"
+                      (x: _) -> x
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x]= x
+maximum' (x: xs) 
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+-- replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n-1)x
+
+-- take
+-- take' :: (Num i, Ord i) => i -> [a] -> [a]
+-- take' n 
+--      | n <= 0  = []
+-- take' _ [] = []
+-- take' n (x: xs) = x : take' (n-1)xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x: xs) = reverse' xs ++ [x]
+
+-- repeat' :: a -> [a]
+-- repeat' x = x: repeat' x
+--
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' []_ = []
+zip' (x: xs)(y: ys) = (x, y): zip' xs ys
+ 
 main =  do 
   print (capital "Dracula")
   print (capital "Yes")
   print (densityTell 0)
   print (let square x = x * x in (square 5, square 3, square 2))
+  print (maximum' [1, 2, 3, 4 ,5])
+  print (replicate' 5 5)
+  print (reverse' [1, 2, 3])
+  print (zip' [1,2,3] ['a', 'b', 'c'])
